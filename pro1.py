85663bc67718fb0195cbca23f427977552002682
Aniket9657 <124755066+Aniket9657@users.noreply.github.com>
2025-08-24 13:10:48 +0530

    Added class Practice

---
+++ pro1.py
@@ -0,0 +1,40 @@
+# class Car:
+#     def __init__(self,brand , model):
+#         self.brand=  brand 
+#         self.model = model
+
+
+
+#   def display(self):
+#        print(f"{self.brand} {self.model}")
+
+
+# C= Car("Ford","Mustang")
+# C.display()
+
+# class Plane :
+#    def __init__(self,type,model):
+#          self.type= type
+#          self.model=model
+
+#    def display(self):
+#        print(f"{self.type} {self.model}")
+
+# P = Plane("Mig","21")
+# P.display()
+
+
+
+
+
+class Food:
+    def __init__ (self,name,price):
+        self.name=name
+        self.price=price
+
+    def show(self):
+         print(f"{self.name} {self.price}")
+
+
+F = Food("Upma",30)
+F.show()       
