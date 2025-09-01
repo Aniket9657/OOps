6db2e4f90567a7f1528ec68514c3a7a1cfb8b181
Aniket9657 <124755066+Aniket9657@users.noreply.github.com>
2025-09-01 20:59:57 +0530

    Custom class

---
+++ Hand.py
@@ -0,0 +1,23 @@
+def __init__(self):
+    print("Hand initiate ")
+
+def Finger(finger:int):
+     if finger == 1:
+        return  print("Sudarshan Chakra")
+
+     elif finger==2 :
+        return print(" Peace ") 
+
+     elif finger==3 :
+         return print("German Three")   
+
+     elif finger ==4 :
+         
+         return print("No Thumb")
+
+
+     elif finger == 5:
+        return  print(" its a hand")    
+         
+        
+Finger(2)  
