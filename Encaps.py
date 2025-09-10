de472df9f627a026db8550a184389ca11d4b7693
Aniket9657 <124755066+Aniket9657@users.noreply.github.com>
2025-09-10 23:49:33 +0530

    Encapsulation

---
+++ Encaps.py
@@ -0,0 +1,14 @@
+class BankAccount:
+    def __init__(self, balance):
+        self.__balance = balance   # private variable
+
+    def deposit(self, amount):
+        self.__balance += amount
+
+    def get_balance(self):
+        return self.__balance
+
+account = BankAccount(1000)
+account.deposit(500)
+print(account.get_balance())   # 1500
+# print(account.__balance)   (will give error - private variable)
